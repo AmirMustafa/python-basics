66683cd88e2817a66cfab4963eed569534c85ce5
Amir Mustafa <amirengg15@gmail.com>
2026-04-15 17:39:50 +0530

    feat: condional operators - not and or

---
+++ 7_conditional_operators.py
@@ -0,0 +1,64 @@
+x = 7
+y = 8
+z = 0
+
+result1 = x == y
+result2 = y > x
+result3 = z < x + 2 # same as z < (x + 2) - it checks left and right of <
+
+result4 = result1 and not result2 and result3
+print(result4)
+
+
+# x = 7
+# y = 8
+# z = 0
+
+# result1 = x == y
+# result2 = y > x
+# result3 = z < x + 2 # same as z < (x + 2) - it checks left and right of <
+
+# result4 = not result2
+# # result4 = result1 or not result2 or result3
+# print(result4)
+
+
+# x = 7
+# y = 8
+# z = 0
+
+# result1 = x == y
+# result2 = y > x
+# result3 = z < x + 2 # same as z < (x + 2) - it checks left and right of <
+
+# result4 = result1 or result2 or result3
+# print(result4)
+
+# result = 6 == 6
+# print(result)
+
+
+# print(7.5 == 7)
+# print(7.5 > 7)
+# print(7.0 == 7)
+
+
+
+# print('ab' > 'ad')
+# print('a' > 'Z')
+
+# print(ord('a'), ord('Z')) # ASCII number
+
+
+# x = 'hello'
+# y = 'heLLo'
+    
+# print(x == y)
+# print(x != y)
+    
+    
+    # x = 'hello'
+    # y = 'hello'
+    
+    # print(x == y)
+    # print(x != y)
