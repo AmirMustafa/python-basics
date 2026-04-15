94b82fde8b7875a5f81feca4cfa20a29fb703b62
Amir Mustafa <amirengg15@gmail.com>
2026-04-15 14:58:22 +0530

    feat: string methods - upper, lower, capitalize, count

---
+++ 6_string_methods.py
@@ -0,0 +1,16 @@
+# count
+hello = "heLLO"
+print(hello.lower().count("ll"))
+
+
+# capitalize
+# hello = "heLLO"
+# print(hello.capitalize())
+
+# lower()
+# hello = "heLLO"
+# print(hello.lower())
+
+# upper()
+# hello = "hello".upper()
+# print(hello.upper())
