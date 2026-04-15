4330a0576df28e24883cd1062a3e06ebb9e61b5b
Amir Mustafa <amirengg15@gmail.com>
2026-04-15 13:52:31 +0530

    feat: arithmetic ops

--- 4_arithmeticoperations.py
+++ 4_arithmeticoperations.py
@@ -1,14 +1,34 @@
-q = 9
+
+q = 10
 r = 3
-print(q / r)
+result = q % r
+print(result)
 
 
-x = 9
-y = 5
+# q = 9.8
+# r = 3
+# result = q // r
+# print(result)
 
-print(x - y)
 
-a = 5
-b = 6.5
+# q = 9
+# r = 3
+# result = q ** r
+# print(result)
 
-print(a - b)
+
+# q = 9
+# r = 3
+# result = int((q / r))
+# print(result)
+
+
+# x = 9
+# y = 5
+
+# print(x - y)
+
+# a = 5
+# b = 6.5
+
+# print(a - b)
