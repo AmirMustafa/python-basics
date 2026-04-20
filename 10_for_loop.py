274d8011dcc528e737fab88efc498e464f4f5eb1
Amir Mustafa <amirengg15@gmail.com>
2026-04-20 14:58:31 +0530

    feat: for loop using range, list

---
+++ 10_for_loop.py
@@ -0,0 +1,14 @@
+# looping through list
+x = [1,5 ,546, 23, 12, 1]
+
+for i in range(len(x)):
+    print(i)
+
+
+# # looping through list
+# for i in [1,5 ,546, 23, 12, 1]:
+#     print(i)
+
+# looping through range
+# for i in range(10, -5, -1):
+#     print(i)
