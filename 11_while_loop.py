826f36cf7b6eb55d7cae1ad91499cb583eb1d42f
Amir Mustafa <amirengg15@gmail.com>
2026-04-20 15:25:09 +0530

    feat: while loop

---
+++ 11_while_loop.py
@@ -0,0 +1,14 @@
+# adding break in while LookupError 
+i = 0
+while True:
+    print('run')
+    i += 1
+    if i == 3:
+        break
+
+
+# # basic while loop
+# i = 0
+# while i < 10:
+#     print('run')
+#     i += 1
