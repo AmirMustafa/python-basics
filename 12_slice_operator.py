fd9a003d7aa42b64fef887ede9c824a3cc6c65de
Amir Mustafa <amirengg15@gmail.com>
2026-04-21 10:20:50 +0530

    feat: slice operator - part 2

--- 12_slice_operator.py
+++ 12_slice_operator.py
@@ -1,12 +1,24 @@
 x = [5, 12, 8, 21, 3, 17, 9, 30]
 names = ["Amir", "Rahul", "Sneha", "John", "Fatima", "Amit", "Sara"]
+s = "hello"
+
+sliced4 = x[::-1] # reverse a list
+sliced5 = names[::-1] # reverse a list
+sliced6 = s[::-1] # reverse a list
+
+
+sliced6 = (1,2,3,4,5)[::-1] # slice also works in tuples
+print(sliced6)
+# print(sliced5)
+# print(sliced6)
+
 
 sliced = x[0: 4: 2]  # will print from 0 to 4(excluding 4)
 sliced2 = x[:4]  # start at index 0 end at index 4 (excluding 4)
 sliced3 = x[1:]  # start at index 1 end at last index (excluding last)
 
-print(sliced2)
-print(sliced3)
+# print(sliced2)
+# print(sliced3)
 
 
 # sliced2 = x[start: stop]
