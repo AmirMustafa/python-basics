ea593c1809ffc43664b29181c64e0f1704c08308
Amir Mustafa <amirengg15@gmail.com>
2026-04-22 13:35:55 +0530

    sets - part 1

--- 13_set_operator.py
+++ 13_set_operator.py
@@ -1,8 +1,9 @@
 x = set()
+s1 = {4,3,2,2,2,2}
+s2 = [3, 4, 213, 44, 1, 2, 2]
 
-y = {4,3,2,2,2,2}
-z = {}
+print(2 in s1)
+print(3 in s2)
 
-y.add(10)
-# y.remove(4)
-print(10 in y)
+# y.add(10)
+# # y.remove(4)
