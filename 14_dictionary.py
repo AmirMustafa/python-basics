737f35f564feac3fa5de4c592195b8812b6dd0e5
Amir Mustafa <amirengg15@gmail.com>
2026-04-22 14:41:43 +0530

    feat: dictionary for in loops

--- 14_dictionary.py
+++ 14_dictionary.py
@@ -6,8 +6,18 @@ x['key2'] = 50
 x[100] = 5
 x[101] = [1,2,3]
 
-del x['key2']
-print(x)
+for key in x:
+    print(key, x[key])
+
+# for key, value in x.items():
+#     print(key, value)
+    
+# for key in x:
+#     print(key)
+
+
+# del x['key2']
+# print(x)
 
 # print(x['key'])
 # print('key' in x)
