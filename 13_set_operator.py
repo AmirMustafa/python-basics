47e95af84992736bf9ed3ea364f12d8cff7ad273
Amir Mustafa <amirengg15@gmail.com>
2026-04-22 13:51:40 +0530

    feat: set - union, difference, intersection

--- 13_set_operator.py
+++ 13_set_operator.py
@@ -1,9 +1,14 @@
 x = set()
 s1 = {4,3,2,2,2,2}
-s2 = [3, 4, 213, 44, 1, 2, 2]
+s2 = {4, 213, 44, 1, 2, 2}
 
-print(2 in s1)
-print(3 in s2)
+print(s1.union(s2))
+print(s1.difference(s2))
+print(s1.intersection(s2))
+
+
+# print(2 in s1)
+# print(3 in s2)
 
 # y.add(10)
 # # y.remove(4)
