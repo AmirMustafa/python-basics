9dd043b7dc8ef977e51dcdeb253806c106a54c08
Amir Mustafa <amirengg15@gmail.com>
2026-04-22 14:26:55 +0530

    feat: dictionary - part 2

--- 14_dictionary.py
+++ 14_dictionary.py
@@ -6,5 +6,10 @@ x['key2'] = 50
 x[100] = 5
 x[101] = [1,2,3]
 
-print(x['key'])
+del x['key2']
 print(x)
+
+# print(x['key'])
+# print('key' in x)
+# print(list(x.values()))
+# print(list(x.keys()))
