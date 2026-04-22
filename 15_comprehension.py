fec4f47a6711e8e0a81bbd30d3e6453bc7bf00a2
Amir Mustafa <amirengg15@gmail.com>
2026-04-22 14:56:45 +0530

    feat: compregension feature of python

---
+++ 15_comprehension.py
@@ -0,0 +1,15 @@
+x1 = [x1 for x1 in range(5)]
+
+x2 = [x2 + 5 for x2 in range(5)]
+
+x4 = [x4 % 2 for x4 in range(5)]
+
+x3 = [0 for x3 in range(5)]
+
+x5 = [i for i in range(100) if i % 2 == 0]
+
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+print(x5)
