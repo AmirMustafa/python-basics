010a471bc55bf832278b0b68193e8018eee6b147
Amir Mustafa <amirengg15@gmail.com>
2026-04-23 19:59:17 +0530

    feat: lambda in python - part 2 - map, filter, lambda

--- 20_lambda.py
+++ 20_lambda.py
@@ -1,6 +1,17 @@
-multiply = lambda x, y: x * y
+x = [1, 23, 23, 2, 34, 56,657, 23, 234, 234,234]
+mp = filter(lambda i: i % 2 == 0, x)
 
-print(multiply(5, 4))
+print(list(mp))
+
+# x = [1, 23, 23, 2, 34, 56,657, 23, 234, 234,234]
+# mp = map(lambda i: i + 2, x)
+
+# print(list(mp))
+
+
+# multiply = lambda x, y: x * y
+
+# print(multiply(5, 4))
 
 
 # square = lambda x: x * x
