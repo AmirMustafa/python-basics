6129af095e3c7736e98f70fd2b2b8a6912bb987d
Amir Mustafa <amirengg15@gmail.com>
2026-04-23 13:53:26 +0530

    feat: unpack operator - *args and **kwargs

--- 17_unpack_operators.py
+++ 17_unpack_operators.py
@@ -1,10 +1,25 @@
-def func(x, y):
-    print(x,y)
+def func(*args, **kwargs):
+    print(args, kwargs)
 
-pairs = [(1,2), (3,4)]
+func(1,2,3,4,5, one=0, two=2)
 
-for pair in pairs:
-    func(*pairs)
+
+# unwrap with dictionary
+
+# def func(x, y):
+#     print(x, y)
+
+# func(**{'x': 2, 'y': 5})
+
+
+# unwrap with lists or tuples
+# def func(x, y):
+#     print(x,y)
+
+# pairs = [(1,2), (3,4)]
+
+# for pair in pairs:
+#     func(*pairs)
 
 
 # def func(*args, **kwargs):
