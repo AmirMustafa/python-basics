22a22be104cdc93f355f8e8f2a17d5e2eeef208c
Amir Mustafa <amirengg15@gmail.com>
2026-04-23 18:41:14 +0530

    feat: global and local scopes in python

---
+++ 18_scope_and_globals.py
@@ -0,0 +1,19 @@
+x = 'amir'
+
+def func(name):
+    global x
+    x = name
+
+print(x)
+func('john')
+print(x)
+
+
+# x = 'amir'
+
+# def func(name):
+#     x = name
+
+# print(x)
+# func('john')
+# print(x)
